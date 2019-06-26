7aaeb4ff15fa623c797ada71c22bc350a3f412d8
Benjamin Rechsteiner <benjamin.rechsteiner@abacus.ch>
2019-06-26 16:46:01 +0200

    add lower function

--- nginx-redis-lookup.lua
+++ nginx-redis-lookup.lua
@@ -89,7 +89,9 @@ end
 
 function contains(list, x)
   for _, v in pairs(list) do
-    if v == x then return true end
+    if string.lower(v) == x then
+      return true
+    end
   end
   return false
 end
